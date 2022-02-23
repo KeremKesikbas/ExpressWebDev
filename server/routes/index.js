6043d994cb2a1ce563ad64f7a1041ac5f16cfb9f
Kerem Kesikbaş <kesikbas.kerem1@gmail.com>
2022-02-23 23:53:49 +0300

    Express Server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var path = require("path");
 
-var db = require("../../lib/sqlite3");
-
+var db = require("../../lib/database");
+    
 var router = express.Router();
 
 path = "path/to/.sqlite";
