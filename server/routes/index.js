5bbc181ea16818295be8d66d0bda779cda44a9f7
Kerem Kesikbaş <kesikbas.kerem1@gmail.com>
2022-02-25 19:16:09 +0300

    mysql added

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 html_path = path.join(__dirname + "/../../resources/");
 
 router.get("/", function(req, res) {
-    res.sendFile(path.join(html_path + "index.html"));
+    res.sendFile(path.join(html_path + "login.html"));
 });
 
 module.exports = router;
